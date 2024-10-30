428ef81f8a3d8e8f831601b448397542ec8fe85e
Prabhat Tripathi <prabhatpati3009@gmail.com>
2024-10-30 18:39:39 +0530

    Initial changes

--- server/database/db.js
+++ server/database/db.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
-
 const Connection = async (username, password) => {
+  const URL = `mongodb+srv://${username}:${password}@crud-app.nia2b.mongodb.net/?retryWrites=true&w=majority&appName=crud-app`;
 
-    const URL = `mongodb+srv://${username}:${password}@curdapp.tv9tcxw.mongodb.net/?retryWrites=true&w=majority`
-
-    try {
-        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
-        console.log('Database connected successfully');
-    } catch (error) {
-        console.log(`Error while connecting to database `, error);
-    }
-
-}
+  try {
+    await mongoose.connect(URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log(`Error while connecting to database `, error);
+  }
+};
 
 export default Connection;
